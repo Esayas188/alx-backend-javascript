69b9ebf6839ad51fe28b767d2a4e227994505c88
Esayas188 <110990267+Esayas188@users.noreply.github.com>
2023-09-26 14:25:16 -0400

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,13 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: "success" }))
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success'
+      };
+    })
     .catch(() => new Error())
-    .finally(() => console.log("Got a response from the API"));
 }
+
+export default handleResponseFromAPI;

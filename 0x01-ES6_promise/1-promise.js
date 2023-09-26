6f28e2abb85c5e8da3e5000ab6e30b3922ec1d35
Esayas188 <110990267+Esayas188@users.noreply.github.com>
2023-09-26 14:22:00 -0400

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,4 @@
-export function getFullResponseFromAPI(success) {
-  // Make a promise based on the success parameter
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
